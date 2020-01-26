4121b9012995ec34698ca7b47275b9c668db61b6
Labodi David <labodidavid@gmail.com>
2020-01-26 22:14:24 +0100

    Homedir setting added

--- musicbotcache.sh
+++ musicbotcache.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
-#Script by: Labodi David // https://daveitservices.tk
+#Script by: Labodi David // https://dit-services.tk
 
 # UNCOMMENT LINES IF YOU NEED
 #SETTINGS
 SINUSPORT1=8067
+HOMEDIR="/home/SinusPort"
 #SINUSPORT2=4918
 #SINUSPORT3=3816
 #SINUSPORT4=3513
-
+# The example is applicable for the /home/SinusPort-8067 directory.
 #Commands
 echo "$(tput setaf 6)Sinusbot cleaning script."
-rm -rf /home/SinusPort-$SINUSPORT1/data/tmp/*
-echo "$(tput setaf 2)/home/SinusPort-$SINUSPORT1/data/tmp/ cleaned!"
-rm -rf /home/SinusPort-$SINUSPORT1/data/cache/*
-echo "$(tput setaf 2)/home/SinusPort-$SINUSPORT1/data/cache/ cleaned!"
-#rm -rf /home/SinusPort-$SINUSPORT2/data/tmp/*
-#echo "$(tput setaf 2)/home/SinusPort-$SINUSPORT2/data/tmp/ cleaned!"
-#rm -rf /home/SinusPort-$SINUSPORT2/data/cache/*
-#echo "$(tput setaf 2)/home/SinusPort-$SINUSPORT2/data/cache/ cleaned!"
-#rm -rf /home/SinusPort-$SINUSPORT3/data/tmp/*
-#echo "$(tput setaf 2)/home/SinusPort-$SINUSPORT3/data/tmp/ cleaned!"
-#rm -rf /home/SinusPort-$SINUSPORT3/data/cache/*
-#echo "$(tput setaf 2)/home/SinusPort-$SINUSPORT3/data/cache/ cleaned!"
-#rm -rf /home/SinusPort-$SINUSPORT4/data/tmp/*
-#echo "$(tput setaf 2)/home/SinusPort-$SINUSPORT4/data/tmp/ cleaned!"
-#rm -rf /home/SinusPort-$SINUSPORT4/data/cache/*
-#echo "$(tput setaf 2)/home/SinusPort-$SINUSPORT4/data/cache/ cleaned!"
+rm -rf $HOMEDIR-$SINUSPORT1/data/tmp/*
+echo "$(tput setaf 2)$HOMEDIR-$SINUSPORT1/data/tmp/ cleaned!"
+rm -rf $HOMEDIR-$SINUSPORT1/data/cache/*
+echo "$(tput setaf 2)$HOMEDIR-$SINUSPORT1/data/cache/ cleaned!"
+#rm -rf $HOMEDIR-$SINUSPORT2/data/tmp/*
+#echo "$(tput setaf 2)$HOMEDIR-$SINUSPORT2/data/tmp/ cleaned!"
+#rm -rf $HOMEDIR-$SINUSPORT2/data/cache/*
+#echo "$(tput setaf 2)$HOMEDIR-$SINUSPORT2/data/cache/ cleaned!"
+#rm -rf $HOMEDIR-$SINUSPORT3/data/tmp/*
+#echo "$(tput setaf 2)$HOMEDIR-$SINUSPORT3/data/tmp/ cleaned!"
+#rm -rf $HOMEDIR-$SINUSPORT3/data/cache/*
+#echo "$(tput setaf 2)$HOMEDIR-$SINUSPORT3/data/cache/ cleaned!"
+#rm -rf $HOMEDIR-$SINUSPORT4/data/tmp/*
+#echo "$(tput setaf 2)$HOMEDIR-$SINUSPORT4/data/tmp/ cleaned!"
+#rm -rf $HOMEDIR-$SINUSPORT4/data/cache/*
+#echo "$(tput setaf 2)$HOMEDIR-$SINUSPORT4/data/cache/ cleaned!"
 
-sleep 2
+sleep 1
 echo "$(tput setaf 2)Sinusbot cache cleaned!"
-echo "$(tput setaf 6)Script by: Labodi David - https://daveitservices.tk"
+echo "$(tput setaf 6)Script by: Labodi David - https://dit-services.tk"
 
 
